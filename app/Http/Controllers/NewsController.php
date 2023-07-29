10d98764c767642199491d14d4e1b357da38eac2
Margarita <msamoray161@gmail.com>
2023-07-29 15:31:09 +0300

    lesson_5 fixed 0.1

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\News;
@@ -10,11 +12,14 @@ use Illuminate\Support\Facades\DB;
 
 class NewsController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(Request $request): View
     {
         $news = app(News::class);
 
-       /* dd(
+        /* dd(
             DB::table('news')
             ->join('categories', 'categories.id', '=', 'news.category_id')
             ->select('news.*', 'categories.title as categoryTitle')
@@ -23,17 +28,59 @@ class NewsController extends Controller
 
         dd(
             DB::table('news')
-                ->whereNotBetween('id', [3,6])
+                ->whereNotBetween('id', [3, 6])
                 /*->where('id', '=', 3)
                 ->Orwhere('title', 'like', '%'. $request->query('q') .'%')*/
                 ->get()
         );
-    }
-
-    public function show(int $id): View
-    {
         return view('admin.news.index', [
             'newsList' => $news->getAll(),
         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('admin.news.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+        return response()->json($request->all());
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return response()->json($this->getNews(), 200);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
