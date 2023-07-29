f179dd606636b4a770207a63e06486e045b8b089
Margarita <msamoray161@gmail.com>
2023-07-29 15:24:20 +0300

    lesson_5

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,22 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\News;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class NewsController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
-return \view('news.index', [
-    'newsList' => $this->getNews(),
-]);
+        $news = app(News::class);
+
+       /* dd(
+            DB::table('news')
+            ->join('categories', 'categories.id', '=', 'news.category_id')
+            ->select('news.*', 'categories.title as categoryTitle')
+            ->get()
+        ); */
+
+        dd(
+            DB::table('news')
+                ->whereNotBetween('id', [3,6])
+                /*->where('id', '=', 3)
+                ->Orwhere('title', 'like', '%'. $request->query('q') .'%')*/
+                ->get()
+        );
     }
 
     public function show(int $id): View
     {
-return \view('news.show', [
-    'news' => $this->getNews($id),
-]);
+        return view('admin.news.index', [
+            'newsList' => $news->getAll(),
+        ]);
     }
 }
